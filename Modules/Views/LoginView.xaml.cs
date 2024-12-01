766072d1b15ed1b808087453579b922d26fad503
T707C <w10188648@usm.edu>
2024-11-30 21:58:07 -0600

    Okay I believe i completed this page, however I do have errors so I am gonna put it in verification and see if that works (i shouldn't have named the test one verificaiton)

--- Modules/Views/LoginView.xaml.cs
+++ Modules/Views/LoginView.xaml.cs
@@ -7,13 +7,42 @@ public partial class LoginView : ContentPage
         InitializeComponent();
     }
 
+    // The Login process 
     private void ProcessLogin(object sender, EventArgs e)
     {
         //Complete Process Login Functionality.  Called by Submit Button
+
+        string username = entryUsername.Text?.Trim() ?? string.Empty;
+        string password = entryPassword.Text ?? string.Empty;
+
+        // Authenticator
+
+        var authResult = App.LoginController.Authenticate(username, password);
+
+        // Error
+
+        if (authResult == Controllers.AuthenticationError.INVALIDUSERNAME)
+        {
+            ShowErrorMessage("Invalid Username. Please try again.");
+            return;
+        }
+
+        else if (authResult == Controllers.AuthenticationError.INVALIDPASSWORD)
+        {
+            ShowErrorMessage("Incorrect Password. Please try again.");
+            return;
+        }
+
+        // Goes to Password List page
+
+        Navigation.PushAsync(new PasswordListView());
     }
 
     private void ShowErrorMessage(string message)
     {
         //Complete ShowError Message functionality.  Supports ProcessLogin.
+
+        labelErrorMessage.Text = message;
+        labelErrorMessage.IsVisible = true;
     }
 }
